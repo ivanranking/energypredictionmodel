4f0aabde5e4ba29167115f33efb76b6598343fd3
orisa ivan <145848664+ivanranking@users.noreply.github.com>
2025-02-28 07:54:14 +0300

    Update deploy.py

--- deploy.py
+++ deploy.py
@@ -90,4 +90,4 @@ def predict():
         return render_template("index.html", prediction_text=f"Error: {str(e)}")
 
 if __name__ == "__main__":
-    app.run(host ='0.0.0.0',port=8080)
+    app.run(host ='0.0.0.0',port=10000)
